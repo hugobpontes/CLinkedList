4faa264eaf818193c0ce52d0edb2705aedecd763
Hugo Pontes <hpontes9@gmail.com>
2023-09-17 13:06:51 +0100

    Add comments and minor aesthetic changes

--- src/linked.c
+++ src/linked.c
@@ -1,18 +1,22 @@
 #include "linked.h"
 #include <stdlib.h>
 
-#define LINKED_ERROR_CODE -1
-
 #define VALID_PTR(p) (p != NULL)
 #define NON_EMPTY_LIST(pList) (pList->pFirst != NULL)
-#define ENTRY_IDX_EXISTS(pList,Idx) (EntryIndex >= 0 && EntryIndex < pList->EntriesNr)
-#define ENTRY_IDX_IS_MAX(pList,Idx) (EntryIndex == pList->EntriesNr)
+#define ENTRY_EXISTS(pList,Idx) (EntryIndex >= 0 && EntryIndex < pList->EntriesNr)
+#define ENTRY_IS_NEW_AT_END(pList,Idx) (EntryIndex == pList->EntriesNr)
 
 #define ENTRY_IS_AT_START(pEntry) (pEntry->pPrevious == NULL)
-
+/* 
+* @brief Creates new linked list entry with input data and empty pointers
+* @param ppEntry Pointer to pointer that will point to new entry
+* @param pVal Pointer to the data to be assigned to new entry
+*/
 static inline void CreateLinkedEntry(LinkedEntry_t** ppEntry, LinkedData_t* pVal){
         *ppEntry = malloc(sizeof(LinkedEntry_t));
         (*ppEntry)->Data = *pVal;
+        (*ppEntry)->pNext=NULL;
+        (*ppEntry)->pPrevious=NULL;
 } 
 /* 
 * @brief Returns pointer to specified linked list entry 
@@ -33,7 +37,17 @@ static inline LinkedEntry_t* GetEntryPtr(LinkedList_t* pList, int EntryIndex){
 
     return pCurrent;
 }
-
+/* 
+* @brief Bypasses a specific element in the linked list
+* @details Given a specific E, its previous entry P and
+* its next entry N: 
+    * makes P's "next" pointer point to N instead of E
+    * makes N's "previous" pointer point to P instead of E
+* some caveats regarding start and end entries apply
+* @param pList Pointer to linked list
+* @param EntryIndex Index of the entry to be bypassed
+* @return Pointer to the bypassed entry
+*/
 static inline LinkedEntry_t* LinkedListBypassEntry(LinkedList_t* pList,int EntryIndex){
     LinkedEntry_t* pEntry;
 
@@ -52,7 +66,15 @@ static inline LinkedEntry_t* LinkedListBypassEntry(LinkedList_t* pList,int Entry
 
     return pEntry;
 }
-
+/* 
+* @brief Manages list entries' pointers so a new entry is inserted before a 
+* specific place in the list (inserts at the end if last index +1 is
+* used in EntryIndex)
+* @param pList Pointer to linked list
+* @param pNewEntry Pointer to new entry
+* @param EntryIndex Index of the entry before which the
+* new one is inserted
+*/
 static inline void LinkedListInsertEntry(LinkedList_t* pList,LinkedEntry_t* pNewEntry,int EntryIndex){
    
     /*(insertion is before old entry)*/
@@ -71,8 +93,10 @@ static inline void LinkedListInsertEntry(LinkedList_t* pList,LinkedEntry_t* pNew
     LinkedEntry_t* pLastEntry;
 
     if (!NON_EMPTY_LIST(pList)){
+        //insert at start if list is empty
         pList->pFirst = pNewEntry;        
     } else if (EntryIndex == pList->EntriesNr){  
+        //insert at the end if input index is max + 1
         pLastEntry = GetEntryPtr(pList,EntryIndex-1);
         pLastEntry->pNext = pNewEntry;
         pNewEntry->pPrevious = pLastEntry;
@@ -105,7 +129,10 @@ void LinkedListInit(LinkedList_t* pList){
         pList->pFirst = NULL;
     }
 }
-
+/* 
+* @brief Deletes and frees all entries in a list
+* @param pList Pointer to the linked list to be cleared
+*/
 void LinkedListClear(LinkedList_t* pList){
     LinkedEntry_t* pCurrent;
     LinkedEntry_t* pTemp;
@@ -123,79 +150,134 @@ void LinkedListClear(LinkedList_t* pList){
 
             pList->EntriesNr--;
         }
+        /*Nothing for list first to point to*/
         pList->pFirst = NULL;
     }
 }
+/*
+* @brief Returns number of entries in the linked list
+* @param pList Pointer to the linked list
+* @return Number of entries in the linked list
+*/
 int LinkedListGetEntriesNr(LinkedList_t* pList){
     return pList->EntriesNr;
 }
-
+/* 
+* @brief Inserts a new entry with new data before a specific place in the list
+* (inserts at the end if last index +1 is used in EntryIndex)
+* @param pList Pointer to linked list
+* @param pData Pointer to data to be added to new inserted entry
+* @param EntryIndex Index of the entry before which the new item is inserted
+*/
 void LinkedListInsert(LinkedList_t* pList, LinkedData_t* pData,int EntryIndex){
     LinkedEntry_t* pNewEntry;
-    if (VALID_PTR(pList) && VALID_PTR(pData) && (ENTRY_IDX_EXISTS(pList,EntryIndex) || ENTRY_IDX_IS_MAX(pList,EntryIndex))){
+    /*To insert at the end of the list, last existing index + 1 is used so it is a valid input*/
+    if (VALID_PTR(pList) && VALID_PTR(pData) && (ENTRY_EXISTS(pList,EntryIndex) || ENTRY_IS_NEW_AT_END(pList,EntryIndex))){
         CreateLinkedEntry(&pNewEntry,pData);
         LinkedListInsertEntry(pList,pNewEntry,EntryIndex);
     }
 }    
-
+/* 
+* @brief Inserts a new entry with new data at the start of the linked list
+* @param pList Pointer to linked list
+* @param pData Pointer to data to be added to new inserted entry
+*/
 void LinkedListInsertStart(LinkedList_t* pList, LinkedData_t* pData){
     LinkedListInsert(pList, pData,0);    
 }
-
+/* 
+* @brief Inserts a new entry with new data at the end of the linked list
+* @param pList Pointer to linked list
+* @param pData Pointer to data to be added to new inserted entry
+*/
 void LinkedListInsertEnd(LinkedList_t* pList, LinkedData_t* pData){
     LinkedListInsert(pList, pData,pList->EntriesNr);    
 }
-
+/* 
+* @brief Reads new data from linked list entry to provided location
+* @param pList Pointer to linked list
+* @param pData Pointer to where linked data is copied to
+* @param EntryIndex Index of the list entry to get data from
+*/
 void LinkedListGet (LinkedList_t* pList, LinkedData_t* pData, int EntryIndex){  
     LinkedEntry_t* pEntry;
 
-    if (VALID_PTR(pList) && VALID_PTR(pData) && NON_EMPTY_LIST(pList) && ENTRY_IDX_EXISTS(pList,EntryIndex)){
+    if (VALID_PTR(pList) && VALID_PTR(pData) && NON_EMPTY_LIST(pList) && ENTRY_EXISTS(pList,EntryIndex)){
         pEntry = GetEntryPtr(pList, EntryIndex);
         *pData = pEntry->Data;
     }
 }
-
+/* 
+* @brief Writes new data to linked list entry from provided location
+* @param pList Pointer to linked list
+* @param pData Pointer to where linked data is copied from
+* @param EntryIndex Index of the list entry to set data in
+*/
 void LinkedListSet (LinkedList_t* pList, LinkedData_t* pData, int EntryIndex){  
     LinkedEntry_t* pEntry;
 
-    if (VALID_PTR(pList) && VALID_PTR(pData) && NON_EMPTY_LIST(pList) && ENTRY_IDX_EXISTS(pList,EntryIndex)){
+    if (VALID_PTR(pList) && VALID_PTR(pData) && NON_EMPTY_LIST(pList) && ENTRY_EXISTS(pList,EntryIndex)){
         pEntry = GetEntryPtr(pList, EntryIndex);
         pEntry->Data = *pData;
     }
 }
-
+/* 
+* @brief Removes entry from list and writes its contents to provided location
+* @param pList Pointer to linked list
+* @param pData Pointer to where linked data is copied to
+* @param EntryIndex Index of the entry to pop
+*/
 void LinkedListPop(LinkedList_t* pList, LinkedData_t* pData, int EntryIndex){
     LinkedEntry_t* pEntry;
 
-    if (VALID_PTR(pList) && NON_EMPTY_LIST(pList) && VALID_PTR(pData) && ENTRY_IDX_EXISTS(pList,EntryIndex)){
+    if (VALID_PTR(pList) && NON_EMPTY_LIST(pList) && VALID_PTR(pData) && ENTRY_EXISTS(pList,EntryIndex)){
         pEntry = LinkedListBypassEntry(pList,EntryIndex);
         *pData = pEntry->Data;
         free(pEntry); 
         pList->EntriesNr--;
     }    
 }
-
+/* 
+* @brief Removes entry from start of list and writes its contents to provided location
+* @param pList Pointer to linked list
+* @param pData Pointer to where linked data is copied to
+*/
 void LinkedListPopStart(LinkedList_t* pList, LinkedData_t* pData){
    LinkedListPop(pList,pData,0); 
 }
+/* 
+* @brief Removes entry from end of list and writes its contents to provided location
+* @param pList Pointer to linked list
+* @param pData Pointer to where linked data is copied to
+*/
 void LinkedListPopEnd(LinkedList_t* pList, LinkedData_t* pData){
    LinkedListPop(pList,pData,pList->EntriesNr-1); 
 }
-
+/* 
+* @brief Removes entry from list
+* @param pList Pointer to linked list
+* @param EntryIndex Index of the entry to remove
+*/
 void LinkedListRemove(LinkedList_t* pList, int EntryIndex){
     LinkedEntry_t* pEntry;
 
-    if (VALID_PTR(pList) && NON_EMPTY_LIST(pList) && ENTRY_IDX_EXISTS(pList,EntryIndex)){
+    if (VALID_PTR(pList) && NON_EMPTY_LIST(pList) && ENTRY_EXISTS(pList,EntryIndex)){
         pEntry = LinkedListBypassEntry(pList,EntryIndex);
         free(pEntry); 
         pList->EntriesNr--;
     }    
 }
-
+/* 
+* @brief Removes entry from start of list
+* @param pList Pointer to linked list
+*/
 void LinkedListRemoveStart(LinkedList_t* pList){
    LinkedListRemove(pList,0); 
 }
-
+/* 
+* @brief Removes entry from end of list
+* @param pList Pointer to linked list
+*/
 void LinkedListRemoveEnd(LinkedList_t* pList){
    LinkedListRemove(pList,pList->EntriesNr-1); 
 }
--- src/linked.h
+++ src/linked.h
@@ -1,12 +1,23 @@
-//Change here to whatever you want to have a linked list of
+/* 
+* @brief Data type of data to be linked (int by default, can be changed in linked.h)
+*/
 typedef int LinkedData_t;
-
+/* 
+* @brief Linked list node
+* @param Data Data stored in the linked node
+* @param pNext Pointer to the next node
+* @param pPrevious Pointer to the previous node
+*/
 typedef struct Linked {
     LinkedData_t Data;
     struct Linked * pNext;
     struct Linked * pPrevious;
 } LinkedEntry_t;
-
+/* 
+* @brief Linked list handle
+* @param pFirst Pointer to the first element in the linked list
+* @param EntriesNr Current number of entries in the linked list
+*/
 typedef struct {
     LinkedEntry_t* pFirst;
     int EntriesNr;
@@ -31,18 +42,4 @@ void LinkedListPopEnd(LinkedList_t* pList, LinkedData_t* pData);
 void LinkedListRemove(LinkedList_t* pList, int EntryIndex);
 void LinkedListRemoveStart(LinkedList_t* pList);
 void LinkedListRemoveEnd(LinkedList_t* pList);
-
-//(all pointer)
-//insert start -done
-//insert -done
-//insert end
-//pop start -done
-//pop -done
-//pop end -done
-//remove start
-//remove 
-//remove end
-
-//set -done
-//get- done
 
